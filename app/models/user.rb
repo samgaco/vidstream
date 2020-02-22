a4c2af2ccea19b44101ea885a48d657d3096097b
samuel <samuelgarciacompanys@gmail.com>
2020-02-23 00:03:38 +0100

    Add validation in purchase model so an item can not be purchased by the same user twice  :rotating_light: Add tests for such validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   has_many :purchases
 
   validates :email, format: { with: /@/ }, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 254 }
+
+  def movies
+    self.purchases.where(purchasable_type: "Movie")
+  end
+
+  def seasons
+    self.purchases.where(purchasable_type: "Season")
+  end
 end
--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Purchase, type: :model do
 
     it 'an item can not be purchased by the same user more than once within 2 days' do
       purchase = Purchase.new(price: 2.99, quality: 'HD', purchasable: movie, user: user)
-      purchase_rep = Purchase.new(price: 2.99, quality: 'HD', purchasable: movie, user: user)
+      purchase_rep = Purchase.new(price: 1.99, quality: 'SD', purchasable: movie, user: user)
       expect { purchase.save }.to change { user.purchases.count }.by(1)
       expect { purchase_rep.save }.to change { user.purchases.count }.by(0)
     end
